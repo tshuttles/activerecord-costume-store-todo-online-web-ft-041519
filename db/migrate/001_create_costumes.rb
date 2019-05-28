40b7093e98f292240451ca3a45fa859f89a9f87f
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-28 23:28:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,4 +10,14 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
   
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price 
+      t.string :url 
+      t.string :size 
+      t.timestamps 
+    end
+  end
+  
 end 
