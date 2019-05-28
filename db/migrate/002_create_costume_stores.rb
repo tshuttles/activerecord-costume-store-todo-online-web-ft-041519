0901531ce4fc69badd24fd277d460964cc6ea9a7
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-28 23:58:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :open 
+      t.datetime :opening_time 
+      t.datetime :closing_time 
+    end
+  end
+  
+end 
