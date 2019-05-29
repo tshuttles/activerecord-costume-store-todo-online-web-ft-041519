caef65e2b036ba661c83b9d114dbac85d4af94a4
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-29 00:02:11 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location 
       t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :open 
+      t.integer :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time 
       t.datetime :closing_time 
     end
